84f59a95659c2aef155d6e6a20b2b675d8c973c8
Andrew S <andy.strickland@gmail.com>
2023-01-20 10:44:07 -0500

    Add Conditionals exercises

---
+++ Conditionals/Stoplight_3.rb
@@ -0,0 +1,34 @@
+# Reformat the following case statement so that it only takes up 5 lines.
+
+# stoplight = ['green', 'yellow', 'red'].sample
+
+# case stoplight
+# when 'green'
+#   puts 'Go!'
+# when 'yellow'
+#   puts 'Slow down!'
+# else
+#   puts 'Stop!'
+# end
+
+stoplight = ['green', 'yellow', 'red'].sample
+# LS Solution:
+case stoplight
+when 'green'  then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+else               puts 'Stop!'
+end
+
+# One thing to keep in mind with this formatting style is that it works best when all of the when clauses have exactly one statement. If you have multiple statements, put them on separate lines, just like the original code:
+
+# case stoplight
+# when 'green'
+#   puts 'Go!'
+#   accelerate
+# when 'yellow'
+#   puts 'Slow down!'
+#   decelerate
+# else
+#   puts 'Stop!'
+#   stop
+# end
